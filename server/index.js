9b5f01e6ead9b53fb1cb1a141a3bcdbb6d1513b9
dburbano-dev <dburbanoh@gmail.com>
2026-05-21 21:42:52 -0500

    Changes: server/index.js port:5000

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const createApp = require('../src/app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = createApp();
 
 app.listen(PORT, () => {
